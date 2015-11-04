260711135f9073a1806b61a3a028e5b098108b65
Mike DuVall <mike.duvall@blackbaud.com>
2015-11-04 07:39:20 -0600

    Updated redhorizon files apache license to mention I changed the files

--- core/src/main/java/redhorizon/filetypes/png/PngPalette.java
+++ core/src/main/java/redhorizon/filetypes/png/PngPalette.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2012, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * Modifications by Mike DuVall
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
